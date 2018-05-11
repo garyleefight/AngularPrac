81b98bf1e15cb96791f496181cd43431ce2a28e4
Gary Li <lisirui@amazon.com>
2018-05-11 15:18:25 -0700

    refractor

--- BookList/src/js/app.js
+++ BookList/src/js/app.js
@@ -1,6 +1,13 @@
 (function (){
     var app = angular.module('helloworld',[]);
+    app.controller('bookCrtl', ['$scope', function($scope){
+        $scope.click = function() {
+            
+        }
+    }]);
     app.directive('hello', function(){
+        
+
         return({
             restrict: 'E',
             template: '<div>Hello World!</div>',
---
+++ BookList/src/js/directives/add-book.js
@@ -0,0 +1,15 @@
+(function() {
+    var addBook = function() {
+        var link = function(scope, element, attr) {
+            scope.addbook = function() {
+
+            }
+        };
+
+        return {
+            restrict: 'A',
+            templateUrl: '',
+            link: link
+        }
+    };
+})();
